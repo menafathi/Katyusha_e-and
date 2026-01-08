fa54147ac63be97c10e73107cf8b23f3d38dd83b
mina fathi <90975341+menafathi@users.noreply.github.com>
2026-01-09 01:45:13 +0200

    Add files via upload

---
+++ worker.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('sw.js')
+      .then(function (reg) {
+        console.log('Service Worker Registered ✔', reg.scope);
+      })
+      .catch(function (err) {
+        console.error('Service Worker Error ❌', err);
+      });
+  });
+}
